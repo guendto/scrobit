ac3c571edc3877c47a8eb9d1dbf38e5eafc8ea37
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2022-01-01 22:35:07 +0200

    refactor(trend.helper): simplify the code

--- frontend/src/helpers/trend.helper.js
+++ frontend/src/helpers/trend.helper.js
@@ -12,7 +12,7 @@
 
 import dayjs from "dayjs";
 import validate from "./validate.helper.js";
-import logger from "../logger.js";
+// import logger from "../logger.js";
 
 /**
  * Determine the longest bearish downward trend.
@@ -25,32 +25,28 @@ const longestBearishDownward = (data) => {
   validate.hasData(data, "prices");
 
   let [prevTime, prev] = data.prices[0];
-  let longestLen = 0;
-  let currLen = 1;
+  let longest = 0;
+  let counter = 1;
 
   data.prices.forEach(([time, curr], index, arr) => {
-    // const isLast = index === arr.length - 1;
-    const isLast = Object.is(arr.length - 1, index); // es6
-    const currDate = dayjs(time);
+    const isLast = Object.is(arr.length - 1, index);
+    const currTime = dayjs(time);
 
-    const ofSameDay = currDate.date() === dayjs(prevTime).date();
-    const isZeroHour = currDate.hour() === 0;
+    const isSameDay = currTime.date() === dayjs(prevTime).date();
+    const isZeroHour = currTime.hour() === 0;
 
-    if ((isZeroHour || isLast) && !ofSameDay) {
-      if (curr <= prev) {
-        currLen += 1;
-      } else {
-        currLen = 1;
-      }
-      if (currLen > longestLen) {
-        longestLen = currLen;
+    if ((isZeroHour || isLast) && !isSameDay) {
+      counter = curr <= prev ? counter + 1 : 1;
+      if (counter > longest) {
+        longest = counter;
       }
     }
+
     prevTime = time;
     prev = curr;
   });
 
-  return longestLen;
+  return longest;
 };
 
 export default { longestBearishDownward };
